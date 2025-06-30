838a7600d299247168ef241d4505f2a4a8f3e4f5
genkimats <genkimatsu24@gmail.com>
2025-06-30 15:46:26 +0900

    setup react app

---
+++ my-app/src/components/Example.js
@@ -0,0 +1,15 @@
+import "./Example.css";
+// ↑↑↑ がこの部品のCSSをインポートするやつ
+
+function Example() {
+
+  // ここにJavaScript関係
+
+  return <div className="App">
+    
+    {/* ここにHTML */}
+    
+    </div>;
+}
+
+export default Example;
