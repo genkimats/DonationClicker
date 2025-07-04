a892a135f7eed9b4de07abc205e5ffbc2b66c442
genkimats <genkimatsu24@gmail.com>
2025-07-05 01:02:17 +0900

    use JSON as database

--- donation-clicker/server/server.js
+++ donation-clicker/server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const fs = require("fs"); // Import the File System module
 
 const app = express();
 app.use(cors());
@@ -14,17 +15,42 @@ const io = new Server(server, {
   },
 });
 
-// In-memory "database" for simplicity
-const users = {};
-let totalDonations = 0;
-let totalChildrenSaved = 0;
-
+const DB_FILE = "./db.json";
 const DONATION_PER_CHILD = 500;
 
+// --- Data Persistence Functions ---
+
+// Reads data from db.json
+const readData = () => {
+  try {
+    const data = fs.readFileSync(DB_FILE, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading from database file:", error);
+    // If the file doesn't exist or is corrupt, return a default structure
+    return { users: {}, totals: { totalDonations: 0, totalChildrenSaved: 0 } };
+  }
+};
+
+// Writes data to db.json
+const writeData = (data) => {
+  try {
+    fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+  } catch (error) {
+    console.error("Error writing to database file:", error);
+  }
+};
+
+// --- Load Initial Data ---
+
+let { users, totals } = readData();
+
+// --- Helper Functions ---
+
 function updateTotals() {
-  const childrenSaved = Math.floor(totalDonations / DONATION_PER_CHILD);
-  if (childrenSaved !== totalChildrenSaved) {
-    totalChildrenSaved = childrenSaved;
+  const childrenSaved = Math.floor(totals.totalDonations / DONATION_PER_CHILD);
+  if (childrenSaved !== totals.totalChildrenSaved) {
+    totals.totalChildrenSaved = childrenSaved;
   }
 }
 
@@ -35,6 +61,8 @@ function getLeaderboard() {
     .map((user) => ({ username: user.username, donated: user.donated }));
 }
 
+// --- Socket.io Logic ---
+
 io.on("connection", (socket) => {
   console.log("a user connected:", socket.id);
 
@@ -43,6 +71,7 @@ io.on("connection", (socket) => {
       return callback({ success: false, message: "Username already taken." });
     }
     users[username] = { username, password, donated: 0, clicks: 0 };
+    writeData({ users, totals }); // <-- Save data to file
     console.log(`User registered: ${username}`);
     callback({ success: true, user: { username, donated: 0, clicks: 0 } });
   });
@@ -61,18 +90,19 @@ io.on("connection", (socket) => {
     if (socket.username && users[socket.username]) {
       users[socket.username].donated += amount;
       users[socket.username].clicks += clicks;
-      totalDonations += amount;
+      totals.totalDonations += amount;
       updateTotals();
+      writeData({ users, totals }); // <-- Save data to file
 
       // Broadcast updates to all clients
       io.emit("updateLeaderboard", getLeaderboard());
-      io.emit("updateTotals", { totalDonations, totalChildrenSaved });
+      io.emit("updateTotals", totals);
     }
   });
 
   // Send initial data on connect
   socket.emit("updateLeaderboard", getLeaderboard());
-  socket.emit("updateTotals", { totalDonations, totalChildrenSaved });
+  socket.emit("updateTotals", totals);
 
   socket.on("disconnect", () => {
     console.log("user disconnected:", socket.id);
